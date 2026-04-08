16cd0ebb7fd497e9d36a7f14d946a2e13876903c
Roman Bagdasarian <roman.d.bagdasarian@gmail.com>
2026-04-08 13:08:52 +0300

    Simulate your circuit locally using statevector or MPS

--- statevector.py
+++ statevector.py
@@ -1,14 +1,61 @@
+import argparse
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
+from helpers.utils import timer
 
 
-simulator = AerSimulator(method="statevector")
-quantum_circuit = QuantumCircuit.from_qasm_file("<YOUR_RELATIVE_PATH>")
+def main():
+    parser = argparse.ArgumentParser(description="Statevector/MPS simulation")
+    parser.add_argument("path", help="Path to .qasm file")
+    parser.add_argument(
+        "--method",
+        choices=["statevector", "matrix_product_state"],
+        default="statevector",
+        help="Simulation method. Default: statevector",
+    )
+    parser.add_argument(
+        "--bond_dim",
+        type=int,
+        default=32,
+        help="Max bond dimension for Matrix Product State. Default: 32",
+    )
+    args = parser.parse_args()
 
-quantum_circuit.measure_all()
-transpiled_circuit = transpile(quantum_circuit, simulator)
+    try:
+        if args.method == "matrix_product_state":
+            simulator = AerSimulator(
+                method="matrix_product_state",
+                matrix_product_state_max_bond_dimension=args.bond_dim,
+            )
+            print(
+                f"Using simulator method: {args.method} with bond dimension {args.bond_dim}"
+            )
+        else:
+            simulator = AerSimulator(method="statevector")
+            print(f"Using simulator method: {args.method}")
 
-job_result = simulator.run(transpiled_circuit, shots=1024).result()
-counts = job_result.get_counts()
+        quantum_circuit = QuantumCircuit.from_qasm_file(args.path)
+        quantum_circuit.measure_all()
+        transpiled_circuit = transpile(quantum_circuit, simulator)
 
-print(max(counts, key=counts.get))
+        with timer("Circuit simulation"):
+            job_result = simulator.run(transpiled_circuit, shots=1024).result()
+        counts = job_result.get_counts()
+
+        peak_bitstring = max(counts, key=counts.get)
+        print(
+            f"Little-Endian: {peak_bitstring}",
+            f"Big-Endian   : {peak_bitstring[::-1]}",
+            sep="\n",
+        )
+        print(f"Probability  : {counts[peak_bitstring] / sum(counts.values())}")
+
+    except FileNotFoundError:
+        print(f"Error: File '{args.path}' not found.")
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
