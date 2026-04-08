678015e23871f67ec9ccde6048593166362e4844
Roman Bagdasarian <roman.d.bagdasarian@gmail.com>
2026-04-08 13:11:21 +0300

    Simulate your circuit with BlueQubit API

--- bluequbit_cpu.py
+++ bluequbit_cpu.py
@@ -1,13 +1,56 @@
+import argparse
 import bluequbit
 from qiskit import QuantumCircuit
 
 
-bq = bluequbit.init("<YOUR_ID>")
-quantum_circuit = QuantumCircuit.from_qasm_file("<YOUR_RELATIVE_PATH>")
+def main():
+    parser = argparse.ArgumentParser(description="Bluequbit CPU simulation")
+    parser.add_argument("path", help="Path to .qasm file")
+    parser.add_argument("--id", required=True, help="Your Bluequbit API Token")
+    parser.add_argument(
+        "--device",
+        default="mps.cpu",
+        help="Target device (mps.cpu or cpu). Default: mps.cpu",
+    )
+    parser.add_argument(
+        "--bond_dim",
+        type=int,
+        default=64,
+        help="Max bond dimension for MPS simulations. Default: 64",
+    )
+    args = parser.parse_args()
 
-quantum_circuit.measure_all()
+    try:
+        bq = bluequbit.init(args.id)
 
-job_result = bq.run(quantum_circuit, device="<YOUR_DEVICE>", shots=1024)
-counts = job_result.get_counts()
+        quantum_circuit = QuantumCircuit.from_qasm_file(args.path)
+        quantum_circuit.measure_all()
 
-print(max(counts, key=counts.get))
+        run_options = {}
+        if "mps" in args.device.lower():
+            run_options["mps_bond_dimension"] = args.bond_dim
+
+        job_result = bq.run(
+            quantum_circuit,
+            device=args.device,
+            shots=1024,
+            options=run_options,
+        )
+        counts = job_result.get_counts()
+
+        peak_bitstring = max(counts, key=counts.get)
+        print(
+            f"Little-Endian: {peak_bitstring}",
+            f"Big-Endian   : {peak_bitstring[::-1]}",
+            sep="\n",
+        )
+
+    except FileNotFoundError:
+        print(f"Error: File '{args.path}' not found.")
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
