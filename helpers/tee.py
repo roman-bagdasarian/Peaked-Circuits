a85e2e3bd456cfdb23f82e2bdd2470696f63599a
Roman Bagdasarian <roman.d.bagdasarian@gmail.com>
2026-04-09 16:15:19 +0300

    Save output to txt

---
+++ helpers/tee.py
@@ -0,0 +1,15 @@
+import sys
+
+
+class Tee:
+    def __init__(self, filename):
+        self.terminal = sys.stdout
+        self.log = open(filename, "w")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
